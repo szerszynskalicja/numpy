d8e650a579d9dacbe8a0efc177d1b42225a93795
szerszynskalicja <szerszynska.alicja@gmail.com>
2022-03-29 12:15:11 +0200

    Added performance test

--- numpy/Tests/Performance/matrix.py
+++ numpy/Tests/Performance/matrix.py
@@ -2,12 +2,41 @@ import numpy as np
 from numpy import emath 
 import time
 from copy import deepcopy
+import random as rm
 
-start_time = time.perf_counter()
-number = np.power(10, 8)
-tmp = np.array(np.arange(0,number, dtype=float), dtype=object)
-tmp2 = deepcopy(tmp)
-end = tmp.dot(tmp2)
-end_time = time.perf_counter()
-timer = end_time - start_time
-print(timer)
+
+def numpy_implementation(arr1, arr2):
+    return np.array(arr1).dot(arr2)
+
+
+def python_implementation(arr1, arr2):
+    result = [[0 for _ in range(len(arr1))] for _ in range(len(arr2[0]))]
+    for i in range(len(arr1)):
+        for j in range(len(arr2[0])):
+            for k in range(len(arr2[0])):
+                result[i][j] += arr1[i][k] * arr2[k][j]
+    return result
+
+
+def generate(size):
+    arr = [[i for i in range(1, size)] for _ in range(1,size)]
+    return arr
+
+
+def tests(power):
+    size = np.power(10, power)
+    tmp = generate(size)
+    tmp2 = deepcopy(tmp)
+    
+    start_time = time.perf_counter()
+    numpy_implementation(tmp, tmp2)
+    timer = time.perf_counter() - start_time
+    print("Test numpy dla wielkości 10^"+str(power)+" czas:"+str(timer))
+    
+    start_time = time.perf_counter()
+    python_implementation(tmp, tmp2)
+    timer = time.perf_counter() - start_time
+    print("Test python dla wielkości 10^"+str(power)+" czas:"+str(timer))
+
+tests(2)
+tests(3)
